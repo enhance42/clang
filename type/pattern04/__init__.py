97a9b553f1255fc3fae19cd57cd5080dd79b3d86
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-03-07 16:41:43 +0530

    tests for pattern04

---
+++ type/pattern04/__init__.py
@@ -0,0 +1,17 @@
+import check50
+
+@check50.check()
+def exists():
+    """pattern04.txt exists"""
+    check50.exists("pattern04.txt")
+    check50.include("expected_pattern.txt")
+
+@check50.check(exists)
+def test():
+    """pattern04.txt contains \"expected pattern\""""
+    correct = open("expected_pattern.txt").read()
+    output = open("pattern04.txt").read()
+    if output == correct:
+        return
+    help_msg = "Make sure you are not giving extra spaces or lines."
+    raise check50.Mismatch(correct, output, help=help_msg)
