3d5768925cd489f9b4d376e037b8b889f59e04bf
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-03-08 14:52:48 +0530

    checks for pattern00 using C, the first program students see in the demo and write

--- makecomputertype/pattern00/__init__.py
+++ makecomputertype/pattern00/__init__.py
@@ -3,9 +3,10 @@ import check50.c
 
 @check50.check()
 def exists():
-    """pattern00.c exists"""
+    """pattern00.c and telugu.h exists"""
     check50.exists("pattern00.c")
     check50.exists("telugu.h")
+    check50.include("expected_output.txt")
 
 @check50.check(exists)
 def compiles():
@@ -13,6 +14,11 @@ def compiles():
     check50.c.compile("pattern00.c", lcs50=True)
 
 @check50.check(compiles)
-def problem04():
-    """pattern00.c prints \"expected pattern\""""
-    check50.run("./pattern00").stdout("42\n\n2 2\n2 2\n\n42\n").exit()
+def test():
+    """check pattern00 printing \"expected pattern\""""
+    correct = open("expected_output.txt").read()
+    output = check50.run("./pattern00")
+    if output == correct:
+        return
+    help_msg = "Make sure you are not giving extra spaces or lines."
+    raise check50.Mismatch(correct, output, help=help_msg)
