a143462e3e91aa65b68469fe62fedb372da5de95
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-03-12 17:13:09 +0530

    finalized, we need three files in directory .e42keys/ like taskname, slugsuffix, urlpath for the shortcuts to work, and displays help message when files are not present

--- docs/2024/addshortcuts.sh
+++ docs/2024/addshortcuts.sh
@@ -1,5 +1,15 @@
-# List of Shortcuts to use in cs50.dev
-# for Programming using C by Enhance42 in March 2024.
+#!/bin/bash
+
+# Check file existence and print a help message if the file does not exist
+check_file_existence() {
+    local file_path="$1"
+    if [ ! -f "$file_path" ]; then
+        echo "You are in folder: $(pwd)"
+        echo "Change to the problem directory"
+        return 1 # Return 1 to indicate failure
+    fi
+    return 0 # Return 0 to indicate success
+}
 
 # to download files for a task
 ewget() {
@@ -9,43 +19,71 @@ ewget() {
 
 # to compile and build
 emake() {
-    echo "make $(cat .keys/taskname)"
-    make $(cat .keys/taskname)
+    local file_path=".e42keys/taskname"
+    check_file_existence "$file_path" || return
+    local taskname=$(cat "$file_path")
+    echo "make $taskname"
+    make $taskname
 }
 
 # to run
 erun() {
-    echo "./$(cat .keys/taskname)"
-    ./$(cat .keys/taskname)
+    local file_path=".e42keys/taskname"
+    check_file_existence "$file_path" || return
+    local taskname=$(cat "$file_path")
+    echo "./$taskname"
+    ./$taskname
 }
 
 # to get help for compilation errors
 ehelp() {
-    echo "help50 make $(cat .keys/taskname)"
-    help50 make $(cat .keys/taskname)
+    local file_path=".e42keys/taskname"
+    check_file_existence "$file_path" || return
+    local taskname=$(cat "$file_path")
+    echo "help50 make $taskname"
+    help50 make $taskname
 }
 
 # to check style of C code
 estyle() {
-    echo "style50 $(cat .keys/taskname).c"
-    style50 $(cat .keys/taskname).c
+    local file_path=".e42keys/taskname"
+    check_file_existence "$file_path" || return
+    local taskname=$(cat "$file_path")
+    echo "style50 $taskname.c"
+    style50 $taskname.c
 }
 
 # to debug C program
 edebug() {
-    echo "debug50 $(cat .keys/taskname)"
-    debug50 $(cat .keys/taskname)
+    local file_path=".e42keys/taskname"
+    check_file_existence "$file_path" || return
+    local taskname=$(cat "$file_path")
+    echo "debug50 $taskname"
+    debug50 $taskname
 }
 
 # to check correctness
 echeck() {
-    echo "check50 enhance42/clang/2024/$(cat .keys/slugsufix)"
-    check50 enhance42/clang/2024/$(cat .keys/slugsufix)
+    local file_path=".e42keys/slugsuffix"
+    check_file_existence "$file_path" || return
+    local slugsuffix=$(cat "$file_path")
+    echo "check50 enhance42/clang/2024/$slugsuffix"
+    check50 enhance42/clang/2024/$slugsuffix
 }
 
 # to submit code
 esubmit() {
-    echo "submit50 enhance42/clang/2024/$(cat .keys/slugsufix)"
-    submit50 enhance42/clang/2024/$(cat .keys/slugsufix)
+    local file_path=".e42keys/slugsuffix"
+    check_file_existence "$file_path" || return
+    local slugsuffix=$(cat "$file_path")
+    echo "submit50 enhance42/clang/2024/$slugsuffix"
+    submit50 enhance42/clang/2024/$slugsuffix
 }
 
+# to see the instructions
+esteps() {
+    local file_path=".e42keys/urlpath"
+    check_file_existence "$file_path" || return
+    local urlpath=$(cat "$file_path")
+    echo "Instructions: https://enhance42.com/clang/2024/$urlpath"
+}
