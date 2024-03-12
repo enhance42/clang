2f0a135d5003d575be86bd9ce4dc0367f4bb1d5e
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-03-12 13:59:05 +0530

    moving to new place, keep it similar

--- docs/2024/addshortcuts.sh
+++ docs/2024/addshortcuts.sh
@@ -9,11 +9,19 @@ new_aliases=(
 	"alias estyle='bash .scripts/estyle.sh'"
 	"alias esubmit='bash .scripts/esubmit.sh'"
 	"alias echeck='bash .scripts/echeck.sh'"
+	"alias esafe='bash .scripts/esafe.sh'"
 )
 
-# clear the contents of .bashrc
-echo > ~/.bashrc
+# clear the contents of .bashrc file and
+# Add function to download assignments
+echo '# shortcut to download assignments
+ewget() {
+    echo "wget https://enhance42.com/clang/$1"
+    wget "https://enhance42.com/clang/$1"
+}' > ~/.bashrc
+
 # Add aliases to .bashrc
+echo '# List of shortcuts to use in cs50.dev' >> ~/.bashrc
 for alias_cmd in "${new_aliases[@]}"; do
     echo "$alias_cmd" >> ~/.bashrc
 done
@@ -21,4 +29,6 @@ done
 # Source the updated .bashrc
 source ~/.bashrc
 
-echo "Aliases added to ~/.bashrc"
+echo "Successfully added SHORTCUTS to your cs50.dev"
+echo "To use shortcuts, run bellow command at the start of the session"
+echo "source ~/.bashrc"
